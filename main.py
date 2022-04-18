6756a8375ce545f698306b5d75b158a8041dbf76
MuspiMerol <wjj123345567@iCloud.com>
2022-04-18 19:32:18 +0800

    partly refactored and made a simple db.

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 from random import shuffle
 from fastapi import FastAPI
+from diskcache import Deque
+from fastapi.responses import RedirectResponse
 from core.fakers import ExperimentItem
 
-# from core.models import ExperimentItem
-
 app = FastAPI()
-experiment_table: list = []
+
+doc = RedirectResponse("/redoc")
 
 
-@app.post("/experiment/new")
-def new_experiment_item(item: ExperimentItem):
-    experiment_table.append(item)
+@app.get("/")
+def home_page():
+    """redirect you to the document page"""
+    return doc
 
 
-@app.get("/test_experiment")
-def get_random_experiment_item():
-    return ExperimentItem()
+class ExperimentAPI:
+    experiment_table = Deque(directory="data")
+    experiment_list = list(experiment_table)
 
+    @staticmethod
+    @app.post("/experiment/new")
+    def new_experiment_item(item: ExperimentItem):
+        ExperimentAPI.experiment_table.append(item)
+        ExperimentAPI.experiment_list.append(item)
 
-@app.get("/experiment/random/{n}")
-def get_random_experiment_items(n: int):
-    shuffle(experiment_table)
-    return experiment_table[:n]
+    @staticmethod
+    @app.put("/experiment/clear")
+    def clear_all_experiments():
+        ExperimentAPI.experiment_table.clear()
+        ExperimentAPI.experiment_list.clear()
+
+    @staticmethod
+    @app.get("/experiment/random/{n}")
+    def get_random_experiment_items(n: int):
+        shuffle(ExperimentAPI.experiment_list)
+        return ExperimentAPI.experiment_list[:n]
+
+    @staticmethod
+    @app.get("/fake_experiment")
+    def get_random_experiment_item():
+        new_item = ExperimentItem()
+        ExperimentAPI.new_experiment_item(new_item)
+        return new_item
