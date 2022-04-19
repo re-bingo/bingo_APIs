cd253e2fa5951ef1dc24077f5f950ac0aeaf6885
MuspiMerol <wjj123345567@iCloud.com>
2022-04-19 22:12:21 +0800

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from sys import path
 from functools import cache
 from fastapi import FastAPI
-from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import FileResponse
 from core import experiment_router, scale_router
+from fastapi.middleware.gzip import GZipMiddleware
 
+path.append("./data")
 app = FastAPI(title="bingo APIs", description="python sever powered by FastAPI")
 app.add_middleware(GZipMiddleware, minimum_size=1024)
 
@@ -11,7 +13,7 @@ app.include_router(scale_router, prefix="/scales", tags=["scales"])
 app.include_router(experiment_router, prefix="/experiments", tags=["experiments"])
 
 
-@app.get("/{file:path}", name="get_static_file")
+@app.get("/{file}", name="get_static_file")
 @cache
 def get_static_file(file: str):
     return FileResponse(f"./data/{file or 'home.html'}")
