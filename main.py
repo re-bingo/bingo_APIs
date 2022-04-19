93827b311c9808c49ccb891762337f33b0c1bc73
MuspiMerol <wjj123345567@iCloud.com>
2022-04-19 20:22:27 +0800

    simplified getting static file implement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from functools import cache
 from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import FileResponse
@@ -6,21 +7,10 @@ from core import experiment_router
 app = FastAPI(title="bingo APIs", description="python sever powered by FastAPI")
 app.add_middleware(GZipMiddleware, minimum_size=1024)
 
-
-@app.get("/")
-def home_page():
-    """redirect you to the document page"""
-    return FileResponse("data/home.html")
-
-
-@app.get("/logo_rounded.png")
-def get_logo():
-    return FileResponse("data/logo_rounded.png")
-
-
-@app.get("/default.css")
-def get_css():
-    return FileResponse("data/default.css")
-
-
 app.include_router(experiment_router, prefix="/experiment", tags=["experiment"])
+
+
+@app.get("/{file:path}", name="get_static_file")
+@cache
+def get_static_file(file: str):
+    return FileResponse(f"data/{file or 'home.html'}")
