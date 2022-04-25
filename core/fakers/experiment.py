cda7eae549cda3170b560b0d34ef188aa5b61577
MuspiMerol <wjj123345567@iCloud.com>
2022-04-26 04:50:42 +0800

    bug fix

--- core/fakers/experiment.py
+++ core/fakers/experiment.py
@@ -45,7 +45,7 @@ class NewExperiment(models.NewExperiment):
 
     def __init__(self, **kwargs):
         params = {
-            "id": "0",
+            "user": "0",
             "title": self.fake_title(),
             "description": self.fake_description(),
             "limit": self.fake_limit(),
