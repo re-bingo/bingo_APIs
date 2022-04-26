199ae705c34bafbcd7e7191f9ecffda7da0dea31
MuspiMerol <wjj123345567@iCloud.com>
2022-04-27 04:41:20 +0800

    simplified fonts.py

--- core/fonts.py
+++ core/fonts.py
@@ -1,24 +1,34 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import Response
 
 app = APIRouter()
 
 
+def load_font(filename):
+    return open(f"./data/fonts/{filename}", "rb").read()
+
+
+regular = load_font("HarmonyOS_Sans_SC_Regular.ttf")
+bold = load_font("HarmonyOS_Sans_SC_Bold.ttf")
+serif = load_font("FZPingXYSK.TTF")
+art = load_font("FZZuoZYXTJW-L.TTF")
+
+
 @app.get("/regular")
 def get_regular_font():
-    return FileResponse("./data/fonts/HarmonyOS_Sans_SC_Regular.ttf")
+    return Response(regular)
 
 
 @app.get("/bold")
 def get_bold_font():
-    return FileResponse("./data/fonts/HarmonyOS_Sans_SC_Bold.ttf")
+    return Response(bold)
 
 
 @app.get("/serif")
 def get_serif_font():
-    return FileResponse("./data/fonts/FZPingXYSK.TTF")
+    return Response(serif)
 
 
 @app.get("/art")
 def get_art_font():
-    return FileResponse("./data/fonts/FZZuoZYXTJW-L.TTF")
+    return Response(art)
