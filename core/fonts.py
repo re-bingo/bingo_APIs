b4961eb5608f9ccb72f7a9b70a0f471eef9073d2
MuspiMerol <wjj123345567@iCloud.com>
2022-04-27 04:30:06 +0800

    new html

--- core/fonts.py
+++ core/fonts.py
@@ -5,15 +5,20 @@ app = APIRouter()
 
 
 @app.get("/regular")
-async def get_regular_font():
+def get_regular_font():
     return FileResponse("./data/fonts/HarmonyOS_Sans_SC_Regular.ttf")
 
 
 @app.get("/bold")
-async def get_bold_font():
+def get_bold_font():
     return FileResponse("./data/fonts/HarmonyOS_Sans_SC_Bold.ttf")
 
 
 @app.get("/serif")
-async def get_serif_font():
+def get_serif_font():
     return FileResponse("./data/fonts/FZPingXYSK.TTF")
+
+
+@app.get("/art")
+def get_art_font():
+    return FileResponse("./data/fonts/FZZuoZYXTJW-L.TTF")
