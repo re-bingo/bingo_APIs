89be05f113b647b10861302310aa2ffe4e5eaba0
MuspiMerol <wjj123345567@iCloud.com>
2022-05-03 18:52:15 +0800

    implement redirecting to avoid nonstandard url

--- core/scales/take.py
+++ core/scales/take.py
@@ -1,4 +1,4 @@
-from fastapi.responses import ORJSONResponse, HTMLResponse, FileResponse
+from fastapi.responses import ORJSONResponse, HTMLResponse, RedirectResponse
 from fastapi import APIRouter, Request
 from random import sample
 from pickle import load
@@ -9,13 +9,13 @@ from starlette.templating import Jinja2Templates
 
 app = APIRouter()
 
-scales = load(open("data/scales.pkl", "rb"))
+scales: dict = load(open("data/scales.pkl", "rb"))
 titles = list(scales)
 flattened = {title: "\n".join("\n".join(div) for div in content.values())
              for title, content in scales.items()}
 
 
-@app.get("", response_model=list[str], response_class=ORJSONResponse)
+@app.get("/", response_model=list[str], response_class=ORJSONResponse)
 async def get_titles():
     """get all the titles on the OBHRM wiki"""
     return titles
@@ -45,17 +45,20 @@ def search_by_content(text: str, n: int = 3) -> list[str]:
 @app.get("/{title}", response_model=dict[str, list[str]], response_class=ORJSONResponse)
 async def get_scale_content(title: str) -> dict[str, list[str]]:
     """get the content of given title (best match)"""
-    key, score, index = extractOne(title, titles)
-    return scales[key]
+    # key, score, index = extractOne(title, titles)
+    return scales.get(title, None) or RedirectResponse(f"/scales/{extractOne(title, titles)[0]}")
 
 
 @app.get("/html/{title}", response_class=HTMLResponse)
 async def get_scale_content(request: Request, title: str):
-    return Jinja2Templates("./data").TemplateResponse(
-        "scale_templates.html",
-        {
-            "request": request,
-            "title": title,
-            "scale": scales[title]
-        }
-    )
+    if (scale := scales.get(title)) is not None:
+        return Jinja2Templates("./data").TemplateResponse(
+            "scale_templates.html",
+            {
+                "request": request,
+                "title": title,
+                "scale": scale
+            }
+        )
+    else:
+        return RedirectResponse(f"/scales/html/{extractOne(title, titles)[0]}")
