aa151c9fe9eab2fff86920a913fe13a3e25a628f
MuspiMerol <wjj123345567@iCloud.com>
2022-05-10 15:37:08 +0800

    implement searching page

--- core/scales/__init__.py
+++ core/scales/__init__.py
@@ -1,1 +1,28 @@
-from .take import app
+from .take import *
+
+
+@app.get("/html/{title}", response_class=HTMLResponse)
+async def get_scale_content(request: Request, title: str):
+    if (scale := scales.get(title)) is not None:
+        return Jinja2Templates("./data").TemplateResponse(
+            "scale_templates.html",
+            {
+                "request": request,
+                "title": title,
+                "scale": scale
+            }
+        )
+    else:
+        return RedirectResponse(f"/scales/html/{extractOne(title, titles)[0]}")
+
+
+@app.get("/html", response_class=HTMLResponse)
+async def page_searching():
+    return open("./data/searching.html", encoding="utf-8").read()
+
+
+@app.get("/{title}", response_model=dict[str, list[str]], response_class=ORJSONResponse)
+async def get_scale_content(title: str) -> dict[str, list[str]]:
+    """get the content of given title (best match)"""
+    # key, score, index = extractOne(title, titles)
+    return scales.get(title, None) or RedirectResponse(f"/scales/{extractOne(title, titles)[0]}")
--- core/scales/take.py
+++ core/scales/take.py
@@ -41,24 +41,3 @@ def search_by_content(text: str, n: int = 3) -> list[str]:
         text, flattened, limit=n, scorer=partial_ratio
     )]
 
-
-@app.get("/{title}", response_model=dict[str, list[str]], response_class=ORJSONResponse)
-async def get_scale_content(title: str) -> dict[str, list[str]]:
-    """get the content of given title (best match)"""
-    # key, score, index = extractOne(title, titles)
-    return scales.get(title, None) or RedirectResponse(f"/scales/{extractOne(title, titles)[0]}")
-
-
-@app.get("/html/{title}", response_class=HTMLResponse)
-async def get_scale_content(request: Request, title: str):
-    if (scale := scales.get(title)) is not None:
-        return Jinja2Templates("./data").TemplateResponse(
-            "scale_templates.html",
-            {
-                "request": request,
-                "title": title,
-                "scale": scale
-            }
-        )
-    else:
-        return RedirectResponse(f"/scales/html/{extractOne(title, titles)[0]}")
